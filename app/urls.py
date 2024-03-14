9ab9eab69e6f9740866bf16e06008c9b967681c2
Rafael Santos Teixeira <rafaelsantost87@gmail.com>
2024-03-14 15:27:03 -0300

    teste de login

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,6 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('teste_login', views.teste_login, name='teste_login'),
+    path('loginurl', views.loginurl, name='loginurl'),
 ]
